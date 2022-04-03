d218d5b117432ad47c1698f42cbe79dba4af3bf0
LeeOSO <550269115@qq.com>
2022-04-03 20:39:09 +0800

    feat: add error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,17 @@ import "./App.css";
 import { AuthenticatedApp } from "./authenticated-app";
 import { UnauthenticatedApp } from "./unauthenticated-app";
 import { useAuth } from "context/auth-context";
+import { ErrorBoundary } from "./components/error-boundary";
+import { FullPageErrorFallback } from "./components/lib";
 
 function App() {
   const { user } = useAuth();
   return (
     <div className="App">
-      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      {/*当子组件出现渲染错误时，统一处理*/}
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}>
+        {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      </ErrorBoundary>
     </div>
   );
 }
