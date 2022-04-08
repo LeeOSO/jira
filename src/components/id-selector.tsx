9c6855a5e51dab0ae869841874158267f7b8fd86
LeeOSO <550269115@qq.com>
2022-04-08 21:50:15 +0800

    feat: add IdSelect

---
+++ src/components/id-selector.tsx
@@ -0,0 +1,45 @@
+import { Raw } from "../utils/types";
+import { Select } from "antd";
+import React from "react";
+
+type SelectProps = React.ComponentProps<typeof Select>; //获取Select所有的Props的类型
+
+interface IdSelectProps
+  extends Omit<SelectProps, "value" | "onChange" | "options"> {
+  //继承组件所有Props类型并移除同名类型
+  value: Raw | null | undefined;
+  onChange: (value?: number) => void;
+  defaultOptionName?: string;
+  options?: { name: string; id: number }[];
+}
+
+/**
+ * value可以传入多种类型，代表默认选择的Option
+ * onChange只会回调 number|undefined 类型
+ * 当 isNaN(Number(value)) 为true的时候，代表选择默认Option
+ * 当选择默认Option时，onChange回调undefined
+ *
+ * @param props
+ * @constructor
+ */
+export const IdSelect = (props: IdSelectProps) => {
+  const { value, onChange, defaultOptionName, options, ...restProps } = props;
+  return (
+    <Select
+      value={toNumber(value)}
+      onChange={(value) => toNumber(value) || undefined}
+      {...restProps}
+    >
+      {defaultOptionName ? (
+        <Select.Option value={0}>{defaultOptionName}</Select.Option>
+      ) : null}
+      {options?.map((option) => (
+        <Select.Option value={option.id} key={option.id}>
+          {option.name}
+        </Select.Option>
+      ))}
+    </Select>
+  );
+};
+
+const toNumber = (value: unknown) => (isNaN(Number(value)) ? 0 : Number(value));
