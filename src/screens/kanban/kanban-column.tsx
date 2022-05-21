a0ba5d95530cd86a7087420788b82816f2160992
LeeOSO <550269115@qq.com>
2022-05-21 22:44:05 +0800

    feat: modify kanban column style

--- src/screens/kanban/kanban-column.tsx
+++ src/screens/kanban/kanban-column.tsx
@@ -1,16 +1,55 @@
 import { Kanban } from "../../types/kanban";
 import { useTasks } from "../../utils/task";
 import { useTasksSearchParams } from "./util";
+import { useTaskTypes } from "../../utils/task-type";
+import taskIcon from "assets/task.svg";
+import bugIcon from "assets/bug.svg";
+import styled from "@emotion/styled";
+import { Card } from "antd";
+
+const TaskTypeIcon = ({ id }: { id: number }) => {
+  const { data: taskTypes } = useTaskTypes();
+  const name = taskTypes?.find((item) => item.id === id)?.name;
+  if (name) {
+    return <img src={name === "task" ? taskIcon : bugIcon} alt={"icon"} />;
+  } else {
+    return null;
+  }
+};
 
 export const KanbanColumn = ({ kanban }: { kanban: Kanban }) => {
   const { data: allTasks } = useTasks(useTasksSearchParams());
   const tasks = allTasks?.filter((task) => task.kanbanId === kanban.id);
   return (
-    <div>
+    <Container>
       <h3>{kanban.name}</h3>
-      {tasks?.map((item) => (
-        <div key={item.id}>{item.name}</div>
-      ))}
-    </div>
+      <TaskContainer>
+        {tasks?.map((item) => (
+          <Card style={{ marginBottom: "0.5rem" }} key={item.id}>
+            <div>{item.name}</div>
+            <TaskTypeIcon id={item.typeId} />
+          </Card>
+        ))}
+      </TaskContainer>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  min-width: 27rem;
+  border-radius: 6px;
+  background-color: rgb(244, 245, 247);
+  display: flex;
+  flex-direction: column;
+  padding: 0.7rem 0.7rem 1rem;
+  margin-right: 1.5rem;
+`;
+
+const TaskContainer = styled.div`
+  overflow: scroll;
+  flex: 1;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
